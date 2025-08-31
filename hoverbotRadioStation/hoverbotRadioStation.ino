2c5ef46c3840ad269c860161bd53a2dd91011ee8
Michasssada <michaslate@gmail.com>
2025-08-31 23:21:32 +0200

    communication with pc and switching to radiolib

--- hoverbotRadioStation/hoverbotRadioStation.ino
+++ hoverbotRadioStation/hoverbotRadioStation.ino
@@ -1,36 +1,25 @@
+#include <RadioLib.h>
 #include <SPI.h>
-#include <LoRa.h>
+
 
 #define SS 15
 #define RST 16
 #define DIO0 5
+SX1278 radio = new Module(SS, DIO0, RST, RADIOLIB_NC);
+struct recieverData{
+  int8_t protocol = 0;
+  bool isConnected = false;
+  uint8_t Rssi = 0;
+  uint8_t status = 0;
 
+}__attribute__((packed));
+recieverData RecieverData;
 void setup() {
   Serial.begin(115200);
-  LoRa.setPins(SS, RST, DIO0);
-  if (!LoRa.begin(433E6)) {
-    while (1); // halt if LoRa init fails
-  }
-  LoRa.setSpreadingFactor(7);
-  LoRa.setSignalBandwidth(125E3);
-  LoRa.setCodingRate4(5);
+  radio.begin();
+  Serial.write((uint8_t*)&RecieverData, sizeof(RecieverData));
 }
 
 void loop() {
-  // Read from LoRa and send to Serial
-  int packetSize = LoRa.parsePacket();
-  if (packetSize > 0) {
-    while (LoRa.available()) {
-      Serial.write(LoRa.read());  // byte-by-byte forwarding
-    }
-  }
-
-  // Read from Serial and send to LoRa
-  if (Serial.available()) {
-    LoRa.beginPacket();
-    while (Serial.available()) {
-      LoRa.write(Serial.read());  // byte-by-byte forwarding
-    }
-    LoRa.endPacket();
-  }
+  Serial.write((uint8_t*)&RecieverData, sizeof(RecieverData));
 }
